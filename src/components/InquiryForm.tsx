ece264c55ce4b121990732f7bc62427af210f8b4
prats69 <pratik@pratsware.com>
2025-06-24 13:27:23 +0530

    Update InquiryForm.tsx

--- src/components/InquiryForm.tsx
+++ src/components/InquiryForm.tsx
@@ -63,10 +63,10 @@ const InquiryForm = () => {
         <div className="grid lg:grid-cols-3 gap-12">
           <div className="lg:col-span-2">
             <Card className="shadow-xl">
-              /* <CardHeader>
+              {/* <CardHeader>
                 <CardTitle className="text-2xl text-gray-900">Inquiry Form</CardTitle>
               </CardHeader>
-              <CardContent> */
+              <CardContent> */}
                 <iframe 
                   frameBorder="0" 
                   style={{height: '1100px', width:'100%', border:'none'}} 
